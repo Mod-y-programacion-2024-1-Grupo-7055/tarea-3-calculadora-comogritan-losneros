f478103ffbf9f08642fa9044a40ae783b0ff3f0e
Ernesto Medina <ernestomid@ciencias.unam.mx>
2023-11-21 22:42:02 -0600

    ya casi

--- src/NodoCos.java
+++ src/NodoCos.java
@@ -1,5 +1,6 @@
 import java.lang.Math;
 /**
+<<<<<<< HEAD
  * @author Ernesto Ibrahim Medina De Luna
  */
 public class NodoCos extends NodoOperador {
@@ -9,14 +10,37 @@ public class NodoCos extends NodoOperador {
 	 */
 	public NodoCos(CompositeEA izq, CompositeEA der) {
 		super(izq, der);
+=======
+ * @author Ernesto Medina
+ * clase NodoCos para el arbol de análisis sintáctico
+ */
+
+public class NodoCos extends NodoOperador {
+
+	/**
+	 * @param izq
+	 * @param der
+	 */
+	public NodoCos(CompositeEA izq, CompositeEA der){
+		super(izq,der);
+>>>>>>> 8fa0e85 (no c pq no jala)
 		precedence=2;
 	}
 
 	/**
+<<<<<<< HEAD
 	 * @return el valor del Cosen del único hijo
+=======
+	 * @return el Coseno del real ingresado, dicho real represemta radianes
+>>>>>>> 8fa0e85 (no c pq no jala)
 	 */
 	@Override
 	public double evalua(){
 		return Math.cos(der.evalua());
 	}
+<<<<<<< HEAD
 }
+=======
+
+}//fin de class
+>>>>>>> 8fa0e85 (no c pq no jala)
--- src/NodoSen.java
+++ src/NodoSen.java
@@ -1,5 +1,6 @@
 import java.lang.Math;
 /**
+<<<<<<< HEAD
  * @author Ernesto Ibrahim Medina De Luna
  */
 public class NodoSen extends NodoOperador {
@@ -9,14 +10,37 @@ public class NodoSen extends NodoOperador {
 	 */
 	public NodoSen(CompositeEA izq, CompositeEA der) {
 		super(izq, der);
+=======
+ * @author Ernesto Medina
+ * clase NodoSen para el arbol de análisis sintáctico
+ */
+
+public class NodoSen extends NodoOperador {
+
+	/**
+	 * @param izq
+	 * @param der
+	 */
+	public NodoSen(CompositeEA izq, CompositeEA der){
+		super(izq,der);
+>>>>>>> 8fa0e85 (no c pq no jala)
 		precedence=2;
 	}
 
 	/**
+<<<<<<< HEAD
 	 * @return el valor del Cosen del único hijo
+=======
+	 * @return el Seno del real ingresado, dicho real represemta radianes
+>>>>>>> 8fa0e85 (no c pq no jala)
 	 */
 	@Override
 	public double evalua(){
 		return Math.sin(der.evalua());
 	}
+<<<<<<< HEAD
 }
+=======
+
+}//fin de class
+>>>>>>> 8fa0e85 (no c pq no jala)
